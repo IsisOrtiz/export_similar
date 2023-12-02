e15850da9e96664276cd2638a4cf315739948f01
Isis Ortiz <isisfortiz@gmail.com>
2023-12-02 20:58:36 -0300

    criação

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import subprocess
+
+# Comando para baixar o modelo 'pt_core_news_sm'
+comando_download = "python -m spacy download pt_core_news_sm"
+
+# Executa o comando no terminal
+subprocess.run(comando_download, shell=True)
+
 from io import StringIO
 import spacy
 import streamlit as st
