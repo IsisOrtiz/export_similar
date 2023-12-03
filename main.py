9b9dd6122ace3d72a00e27f5917f954b00e0b4d1
Isis Ortiz <isisfortiz@gmail.com>
2023-12-03 16:34:38 -0300

    ajuste versão 2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from io import StringIO
+import numpy as np
 import spacy
 import streamlit as st
 import pandas as pd
@@ -16,6 +17,14 @@ indice_predict = st.sidebar.slider(
 
 
 
+def get_sentence_vector(sentence):
+    doc = nlp(sentence)
+    vectors = [word.vector for word in doc if word.has_vector]
+    if vectors:
+        return np.mean(vectors, axis=0)
+    else:
+        return np.zeros_like(nlp.vocab.vectors[0])
+    
 def convert_df(_data):
     df = pd.DataFrame(_data, columns = ['frase1', 'frase2', 'similaridade', 'indice'])
     return df.to_csv(index=False, sep=";").encode('utf-8')
@@ -29,7 +38,7 @@ def getindice():
     )
     return indice_predict
 #funcao para processar os dados
-def process(_data1, _data2):
+def process(_data1, _data2, _cosine_similarities):
     _dados_processados = []
 
     total_iterations_raiz = len(_data1)
@@ -40,17 +49,21 @@ def process(_data1, _data2):
     progress_text = "Por favor agurde o processamento dos dados."
     progess_bar = st.progress(0, text=progress_text)
 
-    for current_iteration_raiz, item1 in enumerate(_data1, start=1):
-        current_progress_raiz = current_iteration_raiz / total_iterations_raiz
+
+    for i, sentence1 in enumerate(_data1):
+
+        current_progress_raiz = i / total_iterations_raiz
         progess_bar_raiz.progress(current_progress_raiz, text=progress_text_raiz)
 
-        for current_iteration, item2 in enumerate(_data2, start=1):
-            current_progress = current_iteration / len(_data2)
+        for j, sentence2 in enumerate(_data2):
+
+            current_progress = j / len(_data2)
             progess_bar.progress(current_progress, text=progress_text)
 
-            similaridade = getsimilarity(item1, item2)
-            if similaridade >= indice_predict:
-                _dados_processados.append([item1, item2, similaridade, indice_predict])
+            similarity = _cosine_similarities[i, j]
+            
+            if similarity >= indice_predict:
+                _dados_processados.append([sentence1, sentence2, similarity, indice_predict])
 
     progess_bar_raiz.empty()
     progess_bar.empty()
@@ -88,7 +101,7 @@ def upload_file1():
 #faz upload do arquivo 2
 def upload_file2():
     # Widget para upload de arquivo
-    uploaded_file = st.file_uploader("Selecione o segundo arquivo txt , até 50MB", type="txt")
+    uploaded_file = st.file_uploader("Selecione o segundo arquivo txt", type="txt")
 
     if uploaded_file is not None:
 
@@ -104,12 +117,30 @@ def upload_file2():
             dados2.append(line)
 
 def main():
-    st.title('Exportação de frases, conforme similaridade - v0.1 !')
+    
+    cosine_similarities = None
+
+    st.title('Exportação de frases, conforme similaridade - v0.2 !')
     st.title(':blue[Isis] :sunglasses:')
     st.write('Similaridade selecionada: ', indice_predict)
     upload_file1()
     upload_file2()
 
+    if len(dados1) > 0 and len(dados2) > 0:
+        # Calculando os vetores médios para cada lista de frases
+        vectors_data1 = [get_sentence_vector(sentence) for sentence in dados1]
+        vectors_data2 = [get_sentence_vector(sentence) for sentence in dados2]
+
+        # Normalizando os vetores
+        vectors_data1_normalized = [vector / np.linalg.norm(vector) for vector in vectors_data1]
+        vectors_data2_normalized = [vector / np.linalg.norm(vector) for vector in vectors_data2]
+
+        # Calculando a similaridade de cosseno para todas as combinações de frases
+        cosine_similarities = np.zeros((len(dados1), len(dados2)))
+
+        for i, vector1 in enumerate(vectors_data1_normalized):
+            for j, vector2 in enumerate(vectors_data2_normalized):
+                cosine_similarities[i, j] = np.dot(vector1, vector2)
 
     col1, col2, col3, col4, col5, col6  = st.columns(6, gap="small")
 
@@ -124,14 +155,14 @@ def main():
             st.write("Não existem dados para processar!")
         else:
             #Processar dados
-            dados_processados = process(dados1, dados2)
+            dados_processados = process(dados1, dados2, cosine_similarities)
             st.write(dados_processados)
     elif btnExport:
         if len(dados1) == 0 or len(dados2) == 0:
             st.write("Não existem dados para processar!")
         else:
             #Processar dados
-            dados_processados = process(dados1, dados2)
+            dados_processados = process(dados1, dados2, cosine_similarities)
             csv = convert_df(dados_processados)
             st.download_button(
             "Press to Download CSV",
